20d9c505683860deaf648c7ecfe37e1da9f7d225
Max Allgaier <max.bowen.allgaier@gmail.com>
2026-06-12 22:40:23 -0400

    Removed unnecessary checks for UserLoginRequest.java

--- src/main/java/me/maxallgaier/synapsis/auth/controller/request/UserLoginRequest.java
+++ src/main/java/me/maxallgaier/synapsis/auth/controller/request/UserLoginRequest.java
@@ -1,15 +1,11 @@
 package me.maxallgaier.synapsis.auth.controller.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record UserLoginRequest(
     @NotBlank
-    @Email
     String email,
 
     @NotBlank
-    @Size(min = 8, max = 30)
     String password
 ) {}
