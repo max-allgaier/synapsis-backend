dee671adcd8ffbba43c4d6029a547f6685262e23
Max Allgaier <max.bowen.allgaier@gmail.com>
2026-06-12 22:50:08 -0400

    Added unsupported http methods to GlobalExceptionHandler.java to return HttpStatus.METHOD_NOT_ALLOWED

--- src/main/java/me/maxallgaier/synapsis/common/GlobalExceptionHandler.java
+++ src/main/java/me/maxallgaier/synapsis/common/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,5 +40,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleMalformedJsonException(HttpMessageNotReadableException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(Map.of("error", "malformed or missing request body"));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
     }
 }
